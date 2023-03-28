eb2b9ef15a5b8b11147e9b56e040b68b527d70a0
_oraklet <olle_wikstrom@hotmail.com>
2023-03-28 10:37:01 +0200

    Added padding on scoreboard page

--- app/scoreboard/page.tsx
+++ app/scoreboard/page.tsx
@@ -3,7 +3,7 @@ import Toplist from "../Toplist";
 
 export default async function scoreBoard() {
 	return (
-		<main className='flex flex-col items-center justify-center pt-12 '>
+		<main className='flex flex-col items-center justify-center p-12 '>
 			<h1 className='text-4xl font-extrabold mb-8'>Top List</h1>
 			<Toplist />
 		</main>
